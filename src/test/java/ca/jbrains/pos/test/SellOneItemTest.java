d85162a6051b4c8e334c62e2013e69321d175d84
J. B. Rainsberger <me@jbrains.ca>
2019-10-25 13:24:46 +0300

    Replaced complicated boolean expression with clearer intent.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -49,9 +49,10 @@ public class SellOneItemTest {
                     "12345", "EUR 7.95", "23456", "EUR 12.50")
                     .toJavaMap();
 
-            if ("12345".equals(barcode) || "23456".equals(barcode))
-                display.setText(pricesByBarcode.get(barcode));
-            else
+            final String price = pricesByBarcode.get(barcode);
+            if (price != null) {
+                display.setText(price);
+            } else
                 display.setText(String.format("Product not found for %s", barcode));
         }
     }
