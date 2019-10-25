9bb3e5ca45ada96a339af4d9b7cc64f806d6f6ca
J. B. Rainsberger <me@jbrains.ca>
2019-10-25 10:18:57 +0300

    We can sell any item, as long as it costs EUR 7.95.

---
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -0,0 +1,29 @@
+package ca.jbrains.pos.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SellOneItemTest {
+
+    @Test
+    void productFound() throws Exception {
+        final Sale sale = new Sale();
+        final Display display = new Display();
+
+        sale.onBarcode("12345");
+
+        Assertions.assertEquals("EUR 7.95", display.getText());
+    }
+
+    private static class Sale {
+        public void onBarcode(String barcode) {
+
+        }
+    }
+
+    private static class Display {
+        public String getText() {
+            return "EUR 7.95";
+        }
+    }
+}
